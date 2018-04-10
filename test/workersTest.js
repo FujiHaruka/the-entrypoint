cff5e49eff9e503f188eeced9af53fa520320399
okunishinishi <okunishinishi@gmail.com>
2018-04-10 14:52:32 +0900

    Releasing new package

--- test/workersTest.js
+++ test/workersTest.js
@@ -5,17 +5,22 @@
 'use strict'
 
 const workers = require('../lib/workers')
-const { ok, equal } = require('assert')
+const {ok, equal} = require('assert')
+const injectmock = require('injectmock')
 
 describe('workers', () => {
   before(() => {
+    injectmock(global, {})
   })
 
   after(() => {
+    injectmock.restoreAll()
   })
 
   it('Do test', () => {
-
+    workers({
+      '/': '/a.js'
+    })
   })
 })
 
